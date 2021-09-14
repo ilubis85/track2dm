3f8ac731ee6ce997e21e0f7a0c2c4ca78bddebdd
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2021-09-14 12:03:50 +0700

    Specify X and Y column

--- R/clearPoint.R
+++ R/clearPoint.R
@@ -3,25 +3,28 @@
 #' @description A function to remove points within a distance.
 #'
 #' @param dataFrame A data-frame contains at least "X" and "Y" in UTM projection.
+#' @param X_col A quoted name of column that consists X coordinates.
+#' @param Y_col A quoted name of column that consists Y coordinates.
 #' @param UTMZone A UTM projection of the target file.
 #' @param distLength A numeric value of a distance
 #'
 #' @return Spatial object data-frame.
 #'
 #' @export
-clearPoint <- function(dataFrame, UTMZone, distLength){
+clearPoint <- function(dataFrame, X_col, Y_col, UTMZone, distLength){
+
   # Specify the coordinate X and Y from a data-frame
-  points <- dataFrame[, c("X", "Y")]
+  points <- dataFrame[, c(X_col, Y_col)]
 
   # Convert to spatial points
-  points_sp <- sp::SpatialPoints(points,proj4string=CRS(as.character(UTMZone)))
+  points_sp <- sp::SpatialPoints(points, proj4string = sp::CRS(as.character(UTMZone)))
 
   # Create distance matrix between points
   points_sp_dist <- rgeos::gDistance(points_sp, byid = T)
 
   # Define the distance threshold
-  points_sp_dist_mtr <- rgeos::gWithinDistance(points_sp, byid = TRUE,
-                                               dist=distLength)
+  points_sp_dist_mtr <- rgeos::gWithinDistance(points_sp, byid = TRUE, dist=distLength)
+
   # Use only diagonal-up
   points_sp_dist_mtr[lower.tri(points_sp_dist_mtr, diag = TRUE)]<- NA
 
