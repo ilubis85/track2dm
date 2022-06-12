3f1413ced3863179baf47af186072146e830561b
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2022-06-12 19:23:44 +0700

    Edit the function to be able to process more than one row of data

--- R/line2points.R
+++ R/line2points.R
@@ -12,30 +12,48 @@
 #' @export
 # Create a function to convert line into an ordered points
 line2points <- function(spLineDF, minDist){
-  # Suppress warning
-  options(warn=-1)
 
-  # Only a single SpatialLinesDataFrame is allowed
+  # Create a function to convert lineS to points, take only a single row
+  line2fun <- function(singleLine){
+
+    # Suppress warning
+    options(warn=-1)
+
+    # Convert SpatialLinesDataFrame into SpatialLines
+    spLine <- as(singleLine, "SpatialLines")
+
+    # Create a regular spaced random points along the line
+    # Number of points is based on the length of line divided by minimum distance between points
+    spPoints <- sp::spsample(x = spLine, n = floor(gLength(spLine)/minDist), type = "regular")
+
+    # Calculate distance from each point
+    pointDist <- rgeos::gProject(spgeom = spLine, sppoint = spPoints,  normalized = FALSE)
+
+    # Return points at specified distance along a line
+    orderID <- rgeos::gInterpolate(spgeom = spLine, d = pointDist, normalized=FALSE) %>% as.data.frame() %>%
+      # Add ID
+      dplyr::transmute(Id = 1:nrow(.), "X" = x, "Y" = y) %>%
+      akar::df2sp(., UTMZone = crs(spLineDF))
+    return(orderID)
+  }
+
+  # Then use the function to convert spatialLinesDataframe into SpatialPointsDataframe
+  # If single line, run the function directly
   if (nrow(spLineDF) == 1){
-    spLineDF_agg <- spLineDF
+    spLineDF_agg <-  line2fun(spLineDF)
 
-    # Stop if data has multiple line
-  } else {stop('Data has multiple rows')}
+  }else {# If more than one line/row, use for loop
 
-  # Convert SpatialLinesDataFrame into SpatialLines
-  spLine <- as(spLineDF_agg, "SpatialLines")
+    # Create a list
+    outPut <- list()
+    for (i in 1:nrow(spLineDF)) {
+      outPut[[i]] <- line2fun(spLineDF[i,])
+    }
+    # Combine the output
+    spLineDF_agg <- do.call(rbind, outPut)
+  }
 
-  # Create a regular spaced random points along the line
-  # Number of points is based on the length of line divided by minimum distance between points
-  spPoints <- sp::spsample(x = spLine, n = floor(gLength(spLine)/minDist), type = "regular")
-
-  # Calculate distance from each point
-  pointDist <- rgeos::gProject(spgeom = spLine, sppoint = spPoints,  normalized = FALSE)
-
-  # Return points at specified distance along a line
-  orderID <- rgeos::gInterpolate(spgeom = spLine, d = pointDist, normalized=FALSE) %>% as.data.frame() %>%
-    # Add ID
-    dplyr::transmute(Id = 1:nrow(.), "X" = x, "Y" = y) %>%
-    akar::df2sp(., UTMZone = crs(utm47n))
-  return(orderID)
+  # Return the result
+  return(spLineDF_agg)
 }
+
