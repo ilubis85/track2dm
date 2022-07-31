7a562d5e1cc454e3878f94a89a6b0acd47cfb190
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2022-07-31 19:09:49 +0700

    Dealth with all NAs

--- R/modal.R
+++ R/modal.R
@@ -10,16 +10,30 @@
 # Function to calculate Modus
 modal <- function(myVector){
 
-  # Replace NA with "-"
-  if (sum(is.na(myVector)) >= TRUE){
+  # Convert to vector
+  myVector <- as.vector(unlist(myVector))
+
+  # If all NA, just extract the first
+  if(sum(is.na(myVector)) == length(myVector)){
+    outPut <- "NA"
+
+  } # If Some NA but not all, replace NA with "-"
+  else if (sum(is.na(myVector)) <= length(myVector)){
     myVector <- tidyr::replace_na(myVector, replace = "NA")
 
-  } else {myVector <- myVector}
+    # Sort myVector
+    myVector <- sort(myVector, na.last = TRUE)
 
-  # Sort myVector
-  myVector <- sort(myVector, na.last = TRUE)
+    # Calculate modal
+    outPut <- names(sort(-table(myVector)))[1]
 
-  # Calculate modal
-  outPut <- names(sort(-table(myVector)))[1]
-  return(outPut)
+  } # Else, put as it is
+  else {
+    # Sort myVector
+    myVector <- sort(myVector, na.last = TRUE)
+
+    # Calculate modal
+    outPut <- names(sort(-table(myVector)))[1]
   }
+  return(outPut)
+}
