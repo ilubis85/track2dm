11d4254175cce4f3f822d57d3a529161dbad2765
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2022-08-08 12:00:25 +0700

    Solved the bugs

--- R/sliceGrids.R
+++ R/sliceGrids.R
@@ -15,17 +15,16 @@
 # Create a function to create random subgrids within main grids
 sliceGrids <- function(mainGrids, mainID, aggreFact, nRandom){
 
-  # Create a function to create subgrids for each main grid cell
-  create_subgrids <- function(spData){
+  if (nrow(mainGrids) == 1){
 
     # calculate the length of one side of the first grid
-    side_length <- rgeos::gLength(spData)/4
+    side_length <- rgeos::gLength(mainGrids)/4
 
     # Create a raster based on aggregate factor
-    raster_cell <- raster::raster(ext=extent(spData), res=side_length/aggreFact)
+    raster_cell <- raster::raster(ext=extent(mainGrids), res=side_length/aggreFact)
 
     # Specify projection
-    crs(raster_cell) <- crs(spData)
+    crs(raster_cell) <- crs(mainGrids)
 
     # Assign values for each cell
     values(raster_cell) <- 1:ncell(raster_cell)
@@ -36,41 +35,76 @@ sliceGrids <- function(mainGrids, mainID, aggreFact, nRandom){
     # Create ID
     subgrid_sp$id <- 1:nrow(subgrid_sp)
 
-    # Combine ID from maingrids
+    # Combine names from the two grids
     subgrid_sp@data <- subgrid_sp@data %>%
-      dplyr::mutate(subgrid_id = paste("gridID", id, sep = "_"))
+      dplyr::mutate(subgrid_id = paste(mainGrids@data[,mainID[1]], id, sep = "_"))
 
     # If RANDOM is TRUE, select random sub-gridcell
     if (nRandom != FALSE){
+
       # randomly select the subgrids
       random_N <- subgrid_sp %>% as.data.frame() %>% dplyr::select(subgrid_id) %>%
         dplyr::sample_n(., nRandom) # Randomly select the ID
 
       # Select randomised cells only
-      output_grids <- subgrid_sp[subgrid_sp$subgrid_id %in% random_N$subgrid_id, ]
+      final_output <- subgrid_sp[subgrid_sp$subgrid_id %in% random_N$subgrid_id, ]
 
     } # If FALSE, do not randomise
-    else {output_grids <- subgrid_sp}
-  }
-
-  # If data has 1 row/feature
-  if (nrow(mainGrids) == 1){
-    final_output <- create_subgrids(spData = mainGrids)
+    else {final_output <- subgrid_sp}
 
   } else { # For multiple rows/features
 
     # Create an output list
     output_grids <- list()
 
+    # Create a list of grid names
+    grid_names <- mainGrids@data[,mainID] %>% unique()
+
     # Iterate the function for all grids available
     for (i in seq_along(mainGrids)) {
-      output_grids[[i]] <- create_subgrids(spData = mainGrids[i,])
+
+      # Subset grid
+      mainGrid_i <- subset(mainGrids, mainGrids@data[,mainID] == grid_names[[i]])
+
+      # calculate the length of one side of the first grid
+      side_length <- rgeos::gLength(mainGrid_i)/4
+
+      # Create a raster based on aggregate factor
+      raster_cell <- raster::raster(ext=extent(mainGrid_i), res=side_length/aggreFact)
+
+      # Specify projection
+      crs(raster_cell) <- crs(mainGrid_i)
+
+      # Assign values for each cell
+      values(raster_cell) <- 1:ncell(raster_cell)
+
+      # Convert raster to polygone
+      subgrid_sp <- rasterToPolygons(raster_cell)
+
+      # Create ID
+      subgrid_sp$id <- 1:nrow(subgrid_sp)
+
+      # Combine names from the two grids
+      subgrid_sp@data <- subgrid_sp@data %>%
+        dplyr::mutate(subgrid_id = paste(grid_names[[i]], id, sep = "_"))
+
+      # If RANDOM is TRUE, select random sub-gridcell
+      if (nRandom != FALSE){
+
+        # randomly select the subgrids
+        random_N <- subgrid_sp %>% as.data.frame() %>% dplyr::select(subgrid_id) %>%
+          dplyr::sample_n(., nRandom) # Randomly select the ID
+
+        # Select randomised cells only
+        output_grids[[i]] <- subgrid_sp[subgrid_sp$subgrid_id %in% random_N$subgrid_id, ]
+
+      } else { output_grids[[i]] <- subgrid_sp}
     }
 
     # Combine the result
     final_output <- do.call(bind, output_grids)
   }
-
   # Return output
   return(final_output)
 }
+
