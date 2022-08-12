0f745e70315a56de9ababe7eb433c95e68804126
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2022-08-12 21:33:50 +0700

    Remove bugs

--- R/modal.R
+++ R/modal.R
@@ -8,18 +8,32 @@
 #'
 #' @export
 # Function to calculate Mode
-# Got the code from https://www.tutorialspoint.com/r/r_mean_median_mode.
+# Modified from https://www.tutorialspoint.com/r/r_mean_median_mode.
 modal <- function(myVector){
 
-  # Get the list of unique value
-  uniq_vec <- base::unique(myVector)
+  # Convert to vector
+  myVector <- as.vector(myVector)
 
-  # Frequent value
-  my_mode <- match(myVector, uniq_vec) %>% tabulate() %>% which.max()
+  # If all NA, return NA
+  if(sum(is.na(myVector))== length(myVector)){
+    outPut <- "NA"
+  }
+  # Else, calculate node
+  else {
 
-  # Extract the most common value
-  outPut <- uniq_vec[my_mode]
+    # Remove any NA values
+    myVector <- myVector[!is.na(myVector)]
 
-  # Return the result
+    # Get the list of unique value
+    uniq_vec <- base::unique(myVector)
+
+    # Frequent value
+    my_mode <- match(myVector, uniq_vec) %>% tabulate() %>% which.max()
+
+    # Extract the most common value
+    outPut <- uniq_vec[my_mode]
+  }
+
+  # Return result
   return(outPut)
 }
