f22db26330d49f5922d7fe99a97d5b10451a3a13
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2023-02-22 12:30:31 +0700

    Edit CRS

--- R/dm2spatial.R
+++ R/dm2spatial.R
@@ -36,7 +36,7 @@ dm2spatial <- function(detectMatrix, proJect){
 
   # Convert to spatial data
   new_detMax_sp <- sp::SpatialPointsDataFrame(coords = new_detMax[,c("X","Y")], data = new_detMax,
-                                              proj4string = raster::crs(proJect))
+                                              proj4string = CRS(proJect))
   # Return output
   return(new_detMax_sp)
 }
