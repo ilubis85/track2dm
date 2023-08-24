cc6abd4e027cd6c1d18d658de67c1ba3e6d82ead
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2023-08-24 21:46:54 +0700

    A new function that combine track and points based on two common columns (e.g., ID and Date)

---
+++ R/track2pts.R
@@ -0,0 +1,84 @@
+#' @title Combine track and waypoints based on two common columns
+#'
+#' @description A function that combine track and waypoints based on two common columns.
+#'
+#' @param trackSp Digitised tracks in spatial dataframe.
+#' @param track_id_1 The first common column in tracks data (e.g., Patrol_ID).
+#' @param track_id_2 The second common column in tracks data (e.g., Patrol_Date).
+#' @param minDist Minimum distance between points.
+#' @param waypointSp Waypoints in spatial dataframe.
+#' @param point_id_1 The first common column in waypoints data (e.g., Patrol_ID).
+#' @param point_id_2 The second common column in waypoints data (e.g., Waypoint_Date).
+#'
+#' @return Spatial points dataframe.
+#'
+#'
+#'
+#' @export
+# Create a function that combine track and waypoints based on two common columns
+track2pts <- function(trackSp, track_id_1, track_id_2, minDist, waypointSp, point_id_1, point_id_2){
+
+  # 1 : SPLIT TRACKS
+  tracks <- list()
+  for (i in 1:nrow(trackSp)) {
+    tracks[[i]] <- trackSp[i,]
+  }
+
+  # 2 : SELECT WAYPOINT BASED ON SELECTED TRACK
+  waypoints <- list()
+  for (k in seq_along(tracks)) {
+
+    # Select tracks
+    track_k <- tracks[[k]]
+
+    # Extract ID from colums
+    id_1 <- track_k@data[, track_id_1]
+    id_2 <- track_k@data[, track_id_2]
+
+    # Then select way point based on track category
+    waypoints[[k]] <- subset(waypointSp, waypointSp@data[,point_id_1] == id_1 &
+                               waypointSp@data[,point_id_2] == id_2)
+  }
+
+  # 3 : CREATE DM FOR EACH COMBINATION OF TRACKS AND WP
+  # Create progress bar
+  pb = progress::progress_bar$new(
+    format = "  processing [:bar] :percent in :elapsed",
+    total = length(tracks), clear = FALSE, width= 60)
+
+  # Output
+  track_pts <- list()
+
+  # Convert each track to multipoints
+  for (i in 1:length(tracks)) {
+
+    # Select individual item
+    tracks_i <- tracks[[i]]
+    waypoints_i <- waypoints[[i]]
+
+    # Add WP_ID for each waypoints, to be copied on the track
+    waypoints_i@data <- waypoints_i@data %>% dplyr::mutate(WP_ID = 1:nrow(.))
+
+    # Convert tracks to multipoints
+    tracks_pts_i <- track2dm::line2points(spLineDF = tracks_i, minDist = 100)
+
+    # Then copy the ID
+    track_pt_wpID <- track2dm::copyID(points1 = tracks_pts_i, points2 = waypoints_i)
+
+    # Copy common columns
+    track_pt_wpID@data <- track_pt_wpID@data %>%
+      dplyr::mutate_at(vars(point_id_1, point_id_2), na_if, y="") %>%
+      tidyr::fill(point_id_1, point_id_2)
+
+    # Combine result
+    track_pts[[i]] <- track_pt_wpID
+
+    # Progress bar
+    pb$tick()
+    Sys.sleep(1 / length(tracks))
+  }
+
+  # 4 : Combine as a result
+  result <- do.call(rbind, track_pts)
+  return(result)
+}
