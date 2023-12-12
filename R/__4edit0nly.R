6dcdbff922cd3392354a77fc92e87948381ecfed
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2023-12-12 15:12:56 +0700

    Rearrange ID in track2pts_2

--- R/__4edit0nly.R
+++ R/__4edit0nly.R
@@ -164,7 +164,98 @@ rgdal::writeOGR(obj = track2pts_1, dsn = "D:/myRpackage/Package_testing/track2dm
                 layer ="track2pts_1a", driver="ESRI Shapefile", overwrite_layer = TRUE)
 
 # Edit track2pts_2 karena tidak ngurut
+track2pts_2a <- function(trackSp, track_id_1, track_id_2, minDist, waypointSp, point_id_1, point_id_2){
 
+  # Convert to sf object
+  tracksf <- sf::st_as_sf(trackSp)
+  waypointsf <- sf::st_as_sf(waypointSp)
+
+  # 1 : SPLIT TRACKS
+  tracks <- list()
+
+  # List of tracks
+  # track_list <- trackSp@data[, c(track_id_1, track_id_2)] %>% unique()
+  track_list <- tracksf %>% sf::st_drop_geometry() %>%
+    dplyr::select(track_id_1, track_id_2) %>% unique()
+
+  for (i in 1:nrow(track_list)) {
+    tracks[[i]]  <- tracksf[i,]
+  }
+
+  # Merge tracks with similar columns
+  print(paste("track_length", length(tracks), sep = " = "))
+
+  # 2 : SELECT WAYPOINT BASED ON SELECTED TRACK
+  waypoints <- list()
+  for (j in seq_along(tracks)) {
+
+    # Suppress warning
+    options(warn=-1)
+
+    # Select tracks
+    track_j <- tracks[[j]]
+
+    # Extract ID from colums
+    id_1 <- track_j[, track_id_1] %>% st_drop_geometry() %>% as.vector()
+    id_2 <- track_j[, track_id_2] %>% st_drop_geometry() %>% as.vector()
+
+    # Then select way point based on track category
+    waypoints[[j]] <- waypointsf %>%
+      filter(across(all_of(point_id_1), ~. == id_1) &
+               across(all_of(point_id_2), ~. == id_2))
+
+  }
+  # 3 : CREATE DM FOR EACH COMBINATION OF TRACKS AND WP
+  # Create progress bar
+  pb = progress::progress_bar$new(
+    format = "  processing [:bar] :percent in :elapsed",
+    total = length(tracks), clear = FALSE, width= 60)
+
+  # Output
+  track_pts <- list()
+
+  # Convert each track to multipoints
+  for (k in 1:length(tracks)) {
+
+    # Suppress warning
+    options(warn=-1)
+
+    # Select individual item
+    tracks_k <- tracks[[k]]
+    waypoints_k <- waypoints[[k]]
+
+    # Add WP_ID for each waypoints, to be copied on the track
+    waypoints_k <- waypoints_k %>% dplyr::mutate(WP_ID = 1:nrow(waypoints_k))
+
+    # Convert tracks to multipoints
+    tracks_pts_k <- track2dm::line2points_2(spLineDF = tracks_k, minDist = minDist)
+
+    # Show plot
+    # plot(st_geometry(tracks_k), col="grey")
+    # plot(st_geometry(waypoints_k), pch=16, cex=0.8, col='black', add=TRUE)
+    # plot(st_geometry(tracks_pts_k), pch=1, col='red', add=TRUE)
+
+    # Then copy the ID
+    track_pt_wpID <- track2dm::copyID_2(points1 = tracks_pts_k, points2 = waypoints_k)
+
+    # Copy common columns
+    track_pt_wpID <- track_pt_wpID %>%
+      dplyr::mutate_at(dplyr::vars(point_id_1, point_id_2), dplyr::na_if, y="") %>%
+      tidyr::fill(point_id_1, point_id_2)
+
+    # Combine result
+    track_pts[[k]] <- track_pt_wpID
+
+    # Progress bar
+    pb$tick()
+    Sys.sleep(1 / length(tracks))
+  }
+  # 4 : Combine as a result
+  result <- do.call(rbind, track_pts) %>%
+    # Rearrange id
+    dplyr::mutate(Id = 1:nrow(.))
+  return(result)
+}
 track2pts_2 <- track2pts_2a(trackSp = trcks_patrol, track_id_1 = "Patrol_ID",
                                    track_id_2 = "Patrol_D", waypointSp = wp_patrol,
                                    point_id_1 = "Patrol_ID", point_id_2 = "Waypoint_D",
