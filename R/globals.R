5eb5c1424f976ed43606e430db8d4113fe7d747d
Muhammad I. Lubis <48168202+ilubis85@users.noreply.github.com>
2024-01-01 14:16:30 +0700

    An R codes to silent the global variables

---
+++ R/globals.R
@@ -0,0 +1,4 @@
+# Used to silent global variables used in the package
+utils::globalVariables(c("Z", "Rep", "value", "name", "X", "Y", "values", "n", ".",
+                         "Subgrid_id", "Replicate", "Presence", "row_number",
+                         "dist_to_prev_point"))
